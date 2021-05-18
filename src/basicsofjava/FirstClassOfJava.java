8045ec8843892cf92bb755cbcb78f69035f0b02c
Admin <Admin@DESKTOP-NDM6CV2>
2021-05-18 17:01:12 +0530

    basic

---
+++ src/basicsofjava/FirstClassOfJava.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+
+public class FirstClassOfJava {
+	public static void main(String[]args) {
+		System.out.println("Velocity");
+		System.out.println("Corporate");
+		System.out.println("training");
+		System.out.println("institute");
+		System.out.println("Velocity Corporate training institute");
+	}
+
+}
